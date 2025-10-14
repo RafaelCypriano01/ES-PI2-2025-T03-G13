754cd1daa81689589ccdb7964f75a86ffa343890
GabiMota12 <gabispmota@gmail.com>
2025-10-13 21:23:18 -0300

    inicio da api cadastro  usuário

---
+++ front/cadastro.js
@@ -0,0 +1,39 @@
+
+const nome         = document.getElementById("nome");
+const sobrenome    = document.getElementById("sobrenome");
+const email        = document.getElementById("email");
+const tel          = document.getElementById("telefone");
+const senha        = document.getElementById("senha");
+const confSenha    = document.getElementById("confirmSenha");
+const btnCadastro  = document.getElementById("botaoCadastro");
+console.log(btnCadastro);
+
+async function salvarUsuario(){                                                   
+    const url = `http://localhost:3000/usuarios`      
+    const body = {
+       nome : nome.value,
+       sobrenome : sobrenome.value,
+       telefone : tel.value,
+       email : email.value,
+       senha : senha.value
+    }
+    fetch("http://localhost:3000/usuarios", {
+  method: "POST", // tipo da requisição
+  headers: {
+    "Content-Type": "application/json", // informa que o corpo é JSON
+  },
+  body: JSON.stringify(body),
+})
+.then(response => {
+  if (!response.ok) {
+    throw new Error("Erro na requisição: " + response.status);
+  }
+  return response.json(); // converte resposta pra JSON
+})
+.then(data => {
+  console.log("Usuário cadastrado:", data);
+})    
+}
+
+btnCadastro.addEventListener("click", salvarUsuario);
+
